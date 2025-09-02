041ca39d65914db418124d873319f24599cff3bb
Hugo Guarneri <hugo.guarneri@global66.com>
2025-09-02 04:43:53 +0000

    revisalo para que se remderice bein la pantallad e Query Editor

--- src/app/(app)/queries/page.tsx
+++ src/app/(app)/queries/page.tsx
@@ -10,10 +10,10 @@ export default function QueriesPage() {
         <DatabaseSchema />
       </div>
       <div className="flex flex-col gap-6 min-h-0">
-        <div className="flex flex-col flex-1 min-h-0">
+        <div className="flex flex-col min-h-0">
           <QueryEditor />
         </div>
-        <div className="flex-1">
+        <div className="flex-1 min-h-0">
           <QueryResults />
         </div>
       </div>
