31e4d7ead1bd7f13afd04e56b8bcfba4f04bd22e
Hugo Guarneri <hugo.guarneri@global66.com>
2025-09-06 02:32:00 +0000

    Hace los ajustes necesarios para que en la pantalla de Query Editor las

--- src/app/(app)/queries/page.tsx
+++ src/app/(app)/queries/page.tsx
@@ -14,13 +14,13 @@ export default function QueriesPage() {
           </p>
         </div>
       </div>
-      <div className="grid grid-cols-1 lg:grid-cols-[350px_1fr] gap-6 min-h-0 flex-1">
-        <div className="flex flex-col gap-6">
-            <DatabaseSchema />
+      <div className="flex flex-col gap-6 flex-1 min-h-0">
+        <div className="grid grid-cols-1 lg:grid-cols-[350px_1fr] gap-6">
+          <DatabaseSchema />
+          <QueryEditor />
         </div>
-        <div className="flex flex-col gap-6 min-h-0">
-            <QueryEditor />
-            <QueryResults />
+        <div className="flex-1 min-h-0">
+          <QueryResults />
         </div>
       </div>
     </div>
