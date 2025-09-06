c6f1e8aeae75c2fcec18950811507174f5cbf2e8
Hugo Guarneri <hugo.guarneri@global66.com>
2025-09-06 02:58:36 +0000

    Tenes que eliminar completamente el scroll horizontal en la pantalla de

--- src/app/(app)/queries/page.tsx
+++ src/app/(app)/queries/page.tsx
@@ -14,14 +14,14 @@ export default function QueriesPage() {
           </p>
         </div>
       </div>
-      <div className="flex flex-col gap-6 flex-1 min-h-0">
-        <div className="grid grid-cols-1 xl:grid-cols-[350px_1fr] gap-6">
-          <DatabaseSchema />
-          <QueryEditor />
-        </div>
-        <div className="flex-1 min-h-0">
-          <QueryResults />
-        </div>
+      <div className="grid grid-cols-1 xl:grid-cols-[350px_1fr] gap-6 flex-1 min-h-0">
+          <div className="flex flex-col gap-6">
+            <DatabaseSchema />
+            <QueryEditor />
+          </div>
+          <div className="flex-1 min-h-0">
+            <QueryResults />
+          </div>
       </div>
     </div>
   );
